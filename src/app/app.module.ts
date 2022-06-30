acff892be654ea20a868cca1f87ccf9ca42b267e
Wavelopper <alnetweb59@gmail.com>
2022-06-30 10:07:38 +0200

    [INITIALISATION] initialisation of project :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NavbarComponent } from './_commons/navbar/navbar.component';
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
