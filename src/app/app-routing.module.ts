f1b7e99140dfb13c14a5c801d8e333cba1fba721
Wavelopper <alnetweb59@gmail.com>
2022-06-30 13:48:57 +0200

    [FEATURE] create path and redirection :sparkles:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component: HomePageComponent},
+  {path:'products', component: ProductsPageComponent},
+  {path: '**', component: NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
